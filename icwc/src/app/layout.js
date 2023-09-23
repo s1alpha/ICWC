0b2270f6cfe69c2554f388f20e8cfa69154c4f5c
Admin <114954853+s1alpha@users.noreply.github.com>
2023-09-23 11:56:24 -0500

    update Site Title

--- icwc/src/app/layout.js
+++ icwc/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-<title>Google Icons</title>
+<title>IronCityWatchCompany</title>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
 </head>
